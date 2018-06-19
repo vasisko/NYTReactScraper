31a6e9a52ffb05e4cec74997e6562e3ca96dc9fa
Carolyn Vasisko <carolyn@mothermedia.com>
2018-06-18 23:53:23 -0500

    in process post

--- scraper-app/routes/api/index.js
+++ scraper-app/routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const articleRoutes = require("./articles");
 
-// Book routes
-router.use("/books", bookRoutes);
+// Article routes
+router.use("/articles", articleRoutes);
 
 module.exports = router;
